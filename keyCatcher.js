2d367e7e17f68ed6a631363cd91ceb14d61d7dec
Sukhwinder Singh <sainisukha88@gmail.com>
2018-08-23 17:24:37 +0000

    Update keyCatcher.js
    moved callback from being directly assigned to listener

--- keyCatcher.js
+++ keyCatcher.js
@@ -15,6 +15,22 @@
 
 "use strict";
 
+/*
+	KeyCatcher 
+
+	It takes three arguments element , keyCode and callBack function.
+
+	CallBack function is executed when the keyCode passed to the function matches the keycode from 
+	event.
+
+	Element  : HtmlElement
+	KeyCode  : Number
+	Callback : Function
+
+*/
+
+
+"use strict";
 
 (function(global) {
 
@@ -25,15 +41,17 @@
 		callback = (typeof(callback) == 'function') && callback;
 
 		if (keyCode && element && callback) {
-			
-			if (element.onkeyup == null) {	
-				element.onkeyup = function(event) {
-					if (event.keyCode == keyCode) {
-						callback(event);
-					}
-				};
+
+			var validateAndExecute = function(event) {
+				if (event.keyCode == keyCode) {
+					callback(event);
+				}
+			}
+
+			if (element.addEventListener) {
+				element.addEventListener('keyup', validateAndExecute);
 			} else {
-				console.error('Element already has an event attached on onkeyup');
+				console.error('Element doesnot have addEventListener');
 			}
 
 		} else {
@@ -44,9 +62,7 @@
 
 
 	if (global.KeyCatcher == null || global.KeyCatcher == undefined) {
-
 		global.KeyCatcher = KeyCatcher;
-
 	}
 
 })(window);
